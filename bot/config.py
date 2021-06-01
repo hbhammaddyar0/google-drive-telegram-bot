1e27d2d4610f0aff118564464cb0cabd44432057
HB Hammad Dyar <66479406+hbhammaddyar0@users.noreply.github.com>
2021-06-01 20:24:09 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,8 +6,8 @@ class config:
     SUDO_USERS = "1160089400" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/K_DramasHindi_bot"
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
-    G_DRIVE_CLIENT_SECRET = ""
+    G_DRIVE_CLIENT_ID = "202264815644.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "X4Z3ca8xfWDb1Voo-F9a7ZxJ"
 
 
 class BotCommands:
